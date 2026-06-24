f21cff72bd98f3ef6da49790394c5f76352f0af7
gobbo_a <alexandre.gobbo@psi.ch>
2026-06-24 14:06:21 +0200

    Separate transport module.

---
+++ src/transport.rs
@@ -0,0 +1,34 @@
+#[derive(Clone, Debug)]
+pub enum Transport {
+    Tcp { port: u32, host: Option< &'static str> },
+    Ipc { name: &'static str },
+}
+
+//pub fn get_local_address() -> String { "127.0.0.1".to_string()}
+// pub fn get_local_address() -> &'static str {"*"}
+pub fn get_local_address() -> &'static str {"0.0.0.0"}
+
+pub fn get_ipc_feeds_folder() -> &'static str {"/tmp"}
+impl Transport {
+    pub fn endpoint(&self) -> String {
+        match self {
+            Transport::Tcp { host, port } => {
+                let host = host
+                    .clone()
+                    .unwrap_or_else(get_local_address);
+
+                if host.contains("://") {
+                    format!("{}:{}", host, port)
+                } else {
+                    format!("tcp://{}:{}", host, port)
+                }
+            },
+            Transport::Ipc { name } => {
+                let folder = get_ipc_feeds_folder();
+                //let path = Path::new(folder.as_str());
+                //fs::create_dir_all(path).expect("Failed to create ipc feeds folder");
+                format!("ipc://{}/bsread_icp_{}", folder, name)
+            },
+        }
+    }
+}
